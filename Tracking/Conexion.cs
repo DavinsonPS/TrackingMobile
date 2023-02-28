a2e2a749477a8e4ea1ec29b07e84f101acf7a1b6
Davinson Argedis Paniagua Santana <davinson.paniagua@correo.tdea.edu.co>
2023-02-28 12:33:53 -0500

    Creación de la clase conexion

---
+++ Tracking/Conexion.cs
@@ -0,0 +1,97 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Tracking
+{
+    internal class Conexion { }
+    
+
+    public class Login
+        {
+      public Login() { }
+      [PrimaryKey, AutoIncrement]
+
+      public int Id { get; set; }
+      [MaxLength(50)]
+
+      public string UserName { get; set; }
+      [MaxLength(20)]
+
+      public string Password { get; set; }
+    }
+
+    public class Auxiliar
+    {
+        static object locker = new object();
+        SQLiteConnection conexion;
+
+        public Auxiliar()
+        {
+            conexion = Conectar();
+            conexion.CreateTable<Login>();
+        }
+
+        public SQLite.SQLiteConnection Conectar()
+        {
+            SQLiteConnection conexionAuxiliar;
+            string nombreArchivo = "tdea.db3"; //nombre de la base de datos
+            string ruta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string rutaCompleta = Path.Combine(ruta, nombreArchivo);
+            conexionAuxiliar = new SQLiteConnection(rutaCompleta);
+            return conexionAuxiliar;
+        }
+
+        #region Manejo de los datos Login
+        //El manejo de los datos
+        //Seleccionar todo
+        public IEnumerable<Login> SeleccionarTodo()
+        {
+            lock (locker)
+            {
+                return (from i in conexion.Table<Login>() select i).ToList();
+            }
+        }
+        //Seleccionar un registro
+        public Login Seleccionar(string userName, string password)
+        {
+            lock (locker)
+            {
+                return conexion.Table<Login>().FirstOrDefault(x => x.UserName == userName && x.Password == password);
+            }
+        }
+        //Actualizar o insertar
+        public int Guardar(Login registro)
+        {
+            lock (locker)
+            {
+                if (registro.Id == 0)
+                {
+                    return conexion.Insert(registro);
+                }
+                else
+                {
+                    return conexion.Update(registro);
+                }
+            }
+        }
+        //Eliminar
+        public int Eliminar(int ID)
+        {
+            lock (locker)
+            {
+                return conexion.Delete<Login>(ID);
+            }
+        }
+        #endregion
+    }
+}
